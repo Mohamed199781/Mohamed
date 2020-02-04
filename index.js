553acca861ca30ebab218f6021df28ca7251403c
Mohamed yasser <60140137+Mohamed199781@users.noreply.github.com>
2020-02-04 14:51:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,128 @@
+/* var name = 'Mohamed';
+console.log(name);
+var number = 22;
+console.log(number);
+var number1 = 1;
+var number2 = 2;
+console.log(number1 == number2);
+console.log('my name is' , name);
+ */
+/* var name,age,jop;
+name='mohamed';
+age=22;
+jop='teacher';
+console.log(name,age,jop);
+
+var noValue;
+
+console.log(noValue);
+
+var testNull = null;
+console.log(testNull);
+
+console.log(typeof name);
+console.log(typeof Number);
+console.log(typeof noValue);
+console.log(typeof testNull);
+
+var x=2;
+x +=10;
+x -=5;
+
+x++;
+x--;
+
+
+console.log(x);
+
+var a=5;
+var b=6;
+
+console.log(a < 10 || b > 10);
+console.log(a == b);
+console.log(!(a == b));
+
+
+var firstname = 'Mohamed';
+var age = 22;
+if (age < 22){
+    console.log(firstname, 'is too young')
+}
+else if(age === 20){
+    console.log(firstname, 'is teenager')
+}
+else {
+    console.log(firstname, 'is old enough')
+};
+
+
+
+switch (true){
+    case age < 22:
+        console.log(firstname, 'young');
+        break;
+        case age === 22:
+        console.log('yung man');
+        break;
+        default:
+            console.log(firstname, 'old enough');
+};
+
+
+var job = "teacher";
+switch (job){  
+case "teacher":
+console.log("teaches kids");
+break;
+case "designer":
+console.log("designs many webs");
+break;
+default:
+console.log("do somesthig else");
+}
+
+
+
+
+age < 25 ? console.log("too young") : console.log("old"); */
+
+
+
+
+
+/* var markHeight = 180 ;
+var markMass = 80 ;
+var johnHeight = 190;
+var johnmass = 90;
+
+var johnBmi = johnmass / (johnHeight*johnHeight);
+console.log(johnBmi);
+
+var markBmi = markMass / (markHeight*markHeight);
+console.log(markBmi);
+
+var name = markBmi>johnBmi;
+console.log(name);
+ */
+
+
+
+/* var johnteam = (10 + 20 + 30) /3
+console.log(johnteam);
+var miketeam = (40 + 50 + 10) /3
+console.log(miketeam);
+
+if (miketeam>johnteam){
+    console.log("miketeam win ");
+}
+
+else if (miketeam<johnteam){
+    console.log("johnteam win");
+}
+
+else {
+    console.log("draw");
+} */
+
+
+
